0abb8183bce555f749ae6664b820a247840b14e8
Danh Nguyen <ncdanhvn@gmail.com>
2024-01-23 12:54:46 +0700

    Fix the bug to get Form view background full page

--- src/pages/Form.tsx
+++ src/pages/Form.tsx
@@ -1,4 +1,4 @@
-import React, { FormEvent, useState } from "react";
+import React, { FormEvent, useRef, useState } from "react";
 import { Box, VStack, Button, Flex, Container } from "@chakra-ui/react";
 import { FormHeader } from "../components/form/FormHeader";
 import { FormTitle } from "../components/form/FormTitle";
@@ -8,6 +8,17 @@ import { FormQuestions } from "../components/form/FormQuestions";
 import { saveAnswers } from "../services/firestoreService";
 
 const Form = () => {
+  const [boxHeight, setBoxHeight] = useState("100vh");
+  const parentRef = useRef<HTMLDivElement>(null);
+  const updateHeight = () => {
+    if (parentRef.current) {
+      const parentHeight = parentRef.current.getBoundingClientRect().height;
+      const viewportHeight = window.innerHeight;
+
+      setBoxHeight(viewportHeight > parentHeight ? "100vh" : "100%");
+    }
+  };
+
   const [answers, setAnswers] = useState({});
 
   const handleAnswerChange = (question: string, value: string | string[]) => {
@@ -27,35 +38,37 @@ const Form = () => {
   };
 
   return (
-    <Box
-      bgImage="./images/party-invitation-bg.webp"
-      height="100%"
-      width="100vw"
-      bgPosition="center"
-      bgRepeat="no-repeat"
-      bgSize="cover"
-      display="flex"
-      alignItems="center"
-      justifyContent="center"
-    >
-      <Container bg="white" borderRadius={"lg"} p={0} overflow={"hidden"}>
-        <VStack gap={0}>
-          <FormHeader />
-          <VStack w={"100%"} gap={4} mb={8}>
-            <FormTitle />
-            <FormDescription />
-            <form onSubmit={handleSubmit}>
-              <FormQuestions handleAnswerChange={handleAnswerChange} />
-              <Flex justifyContent="center" width="full" mt={8}>
-                <Button colorScheme="pink" type="submit">
-                  Submit
-                </Button>
-              </Flex>
-            </form>
+    <div ref={parentRef}>
+      <Box
+        bgImage="./images/party-invitation-bg.webp"
+        height={boxHeight}
+        width="100vw"
+        bgPosition="center"
+        bgRepeat="no-repeat"
+        bgSize="cover"
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Container bg="white" borderRadius={"lg"} p={0} overflow={"hidden"}>
+          <VStack gap={0}>
+            <FormHeader />
+            <VStack w={"100%"} gap={4} mb={8}>
+              <FormTitle />
+              <FormDescription />
+              <form onSubmit={handleSubmit}>
+                <FormQuestions handleAnswerChange={handleAnswerChange} />
+                <Flex justifyContent="center" width="full" mt={8}>
+                  <Button colorScheme="pink" type="submit">
+                    Submit
+                  </Button>
+                </Flex>
+              </form>
+            </VStack>
           </VStack>
-        </VStack>
-      </Container>
-    </Box>
+        </Container>
+      </Box>
+    </div>
   );
 };
 
