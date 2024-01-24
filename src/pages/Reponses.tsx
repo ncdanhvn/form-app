c7dd363707b670e6dc54147130af4224a1ada802
Danh Nguyen <ncdanhvn@gmail.com>
2024-01-24 16:55:27 +0700

    Reponses page

---
+++ src/pages/Reponses.tsx
@@ -0,0 +1,14 @@
+import { Button, Container } from "@chakra-ui/react";
+import React from "react";
+
+const Reponses = () => {
+  return (
+    <Container textAlign={"center"}>
+      <Button mt={6} variant={"link"}>
+        Donwload sheet file
+      </Button>
+    </Container>
+  );
+};
+
+export default Reponses;
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
 import Form from "./pages/Form";
 import CreateForm from "./pages/CreateForm";
+import Reponses from "./pages/Reponses";
 
 const router = createBrowserRouter([
   { path: "/", element: <Form /> },
   { path: "/createForm", element: <CreateForm /> },
+  { path: "/reponses", element: <Reponses /> },
 ]);
 
 export default router;
