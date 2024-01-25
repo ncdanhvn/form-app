063ced0f499385b92ff5eb1722ac8736c68c7d38
Danh Nguyen <ncdanhvn@gmail.com>
2024-01-25 19:55:12 +0700

    Add CanvasEditPanel

---
+++ src/components/canvas/CanvasEditPanel.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import {
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+  Box,
+  Text,
+} from "@chakra-ui/react";
+
+const CanvasEditPanel = () => {
+  return (
+    <Box
+      position="fixed"
+      right="0"
+      top="0"
+      width="300px"
+      height="100vh"
+      bg="white"
+      boxShadow="md"
+    >
+      <Accordion allowMultiple defaultIndex={[0]}>
+        <AccordionItem>
+          <AccordionButton>
+            <Box flex="1" textAlign="left">
+              Background
+            </Box>
+            <AccordionIcon />
+          </AccordionButton>
+          <AccordionPanel pb={4}>
+            {/* Background edit options go here */}
+          </AccordionPanel>
+        </AccordionItem>
+
+        {/* Repeat AccordionItem for each section: Header, Title, Description, Questions */}
+
+        {/* Example for Header */}
+        <AccordionItem>
+          <AccordionButton>
+            <Box flex="1" textAlign="left">
+              Header
+            </Box>
+            <AccordionIcon />
+          </AccordionButton>
+          <AccordionPanel pb={4}>
+            {/* Header edit options go here */}
+          </AccordionPanel>
+        </AccordionItem>
+
+        {/* Add other sections similarly */}
+      </Accordion>
+    </Box>
+  );
+};
+
+export default CanvasEditPanel;
--- src/pages/Canvas.tsx
+++ src/pages/Canvas.tsx
@@ -1,74 +1,43 @@
-import { EditIcon } from "@chakra-ui/icons";
-import {
-  Box,
-  Container,
-  IconButton,
-  Text,
-  useDisclosure,
-} from "@chakra-ui/react";
 import React from "react";
-import BackgroundCustomizationDialog from "../components/BackgroundCustomizationDialog";
+import { Box, Container, Text } from "@chakra-ui/react";
 import { useCanvasStore } from "../stores/canvasStore";
+import CanvasEditPanel from "../components/canvas/CanvasEditPanel"; // Import the CanvasEditPanel component
 
 const Canvas: React.FC = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const { backgroundType, backgroundValue, updateBackground } =
-    useCanvasStore();
+  const { backgroundType, backgroundValue } = useCanvasStore();
+
   return (
     <>
-      <Box
-        height="100vh"
-        width="100vw"
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-        position="relative" // Make the outer box a positioning context
-      >
+      {/* Wrap your existing Box and the CanvasEditPanel in a Flex container */}
+      <Box display="flex" height="100vh" width="100vw">
+        {/* Canvas Content */}
         <Box
           {...(backgroundType === "color"
             ? { bg: backgroundValue }
             : { bgImage: backgroundValue })}
-          position="absolute" // Set to absolute
-          top={0}
-          left={0}
-          height="100%"
-          width="100%"
+          flex="1" // This makes sure the canvas takes up all space except for the edit panel
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
           bgPosition="center"
           bgRepeat="no-repeat"
           bgSize="cover"
-          _hover={{ ".customize-button": { opacity: 1 } }} // Apply hover effect only to this box
         >
-          <IconButton
-            aria-label="Customize Background"
-            icon={<EditIcon />}
-            position="absolute"
-            top="4"
-            left="50%"
-            transform="translate(-50%)"
-            opacity={0}
-            className="customize-button" // Class to target for hover effect
-            onClick={onOpen}
-            transition="opacity 0.3s ease"
-          />
+          <Container
+            bg="white"
+            borderRadius="lg"
+            p={0}
+            overflow="hidden"
+            h={60}
+          >
+            <Text>Some content here</Text>
+          </Container>
         </Box>
 
-        <Container
-          position="relative"
-          bg="white"
-          borderRadius="lg"
-          p={0}
-          overflow="hidden"
-          h={60}
-          zIndex="1"
-        >
-          <Text>Some content here</Text>
-        </Container>
+        <Box width="300px" flexShrink={0} height="100vh">
+          <CanvasEditPanel />
+        </Box>
       </Box>
-      <BackgroundCustomizationDialog
-        isOpen={isOpen}
-        onClose={onClose}
-        updateBackground={(newBg, type) => updateBackground(newBg, type)}
-      />
     </>
   );
 };
