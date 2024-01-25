c0c35f4170ae288f16398a77e63a5de678ec7d3b
Danh Nguyen <ncdanhvn@gmail.com>
2024-01-25 08:56:28 +0700

    Add packages to save submitted answers to file

--- src/pages/Reponses.tsx
+++ src/pages/Reponses.tsx
@@ -1,10 +1,34 @@
 import { Button, Container } from "@chakra-ui/react";
 import React from "react";
+import Excel from "exceljs";
+import { saveAs } from "file-saver";
+
+async function createExcelFile() {
+  // Create a new workbook
+  const workbook = new Excel.Workbook();
+
+  // Add a new worksheet
+  const worksheet = workbook.addWorksheet("My Sheet");
+
+  // Add some data as rows
+  worksheet.addRow(["Name", "Age", "Location"]);
+  worksheet.addRow(["John Doe", 30, "New York"]);
+  worksheet.addRow(["Jane Smith", 28, "London"]);
+
+  // Write to a file
+  const buffer = await workbook.xlsx.writeBuffer();
+  try {
+    saveAs(new Blob([buffer]), "file.xlsx");
+    console.log("Excel file created");
+  } catch (error) {
+    console.log("Error writing excel export", error);
+  }
+}
 
 const Reponses = () => {
   return (
     <Container textAlign={"center"}>
-      <Button mt={6} variant={"link"}>
+      <Button mt={6} variant={"link"} onClick={createExcelFile}>
         Donwload sheet file
       </Button>
     </Container>
