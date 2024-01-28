a4735513738f409801d8dbf3def46db35e542cc9
Danh Nguyen <ncdanhvn@gmail.com>
2024-01-28 17:09:23 +0700

    Add Register page

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -9,6 +9,8 @@ import {
   Link as ChakraLink,
 } from "@chakra-ui/react";
 import { Link as RouterLink } from "react-router-dom";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebaseConfig";
 
 const RegisterPage: React.FC = () => {
   const [username, setUsername] = useState("");
@@ -17,9 +19,40 @@ const RegisterPage: React.FC = () => {
   const [confirmPassword, setConfirmPassword] = useState("");
   const toast = useToast();
 
-  const handleRegister = () => {
-    // Implement registration logic
-    // Check if passwords match, etc.
+  const handleRegister = async () => {
+    if (password !== confirmPassword) {
+      toast({
+        title: "Passwords do not match",
+        status: "error",
+      });
+      return;
+    }
+
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      // User successfully created
+      toast({
+        title: "Account created successfully",
+        description: "Redirect you to the homepage",
+        status: "success",
+      });
+
+      // You can redirect the user to the home page or login page after successful registration
+      // navigate('/home'); // Uncomment and use if you have set up react-router's navigate
+    } catch (error) {
+      toast({
+        title: "Registration failed",
+        description:
+          error instanceof Error
+            ? getErrorDescription(error)
+            : "An unknown error occurred",
+        status: "error",
+      });
+    }
   };
 
   return (
@@ -68,3 +101,21 @@ const RegisterPage: React.FC = () => {
 };
 
 export default RegisterPage;
+
+const getErrorDescription = (error: Error) => {
+  switch (error.message) {
+    case "Firebase: Error (auth/missing-email).":
+      return "Please enter the Email";
+    case "Firebase: Error (auth/invalid-email).":
+      return "Please enter a correct Email";
+    case "Firebase: Error (auth/missing-password).":
+      return "Please enter the Password";
+    case "Firebase: Password should be at least 6 characters (auth/weak-password).":
+      return "Password should be at least 6 characters";
+    case "Firebase: Error (auth/email-already-in-use).":
+      return "Email already in use";
+    default:
+      //   return "Some error occurred";
+      return error.message;
+  }
+};
