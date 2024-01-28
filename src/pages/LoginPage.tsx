a0158fdaf942c224461f49115e2020c24565894e
Danh Nguyen <ncdanhvn@gmail.com>
2024-01-28 16:32:15 +0700

    Refactor LogIn page

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,4 +1,4 @@
-import { Button, Input, VStack, useToast } from "@chakra-ui/react";
+import { Button, Container, Input, VStack, useToast } from "@chakra-ui/react";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import React, { useState } from "react";
 import { auth } from "../firebaseConfig"; // adjust the path as needed
@@ -17,28 +17,50 @@ const LoginPage: React.FC = () => {
       toast({
         title: "Failed to log in",
         description:
-          error instanceof Error ? error.message : "An unknown error occurred",
+          error instanceof Error
+            ? getErrorDescription(error)
+            : "An unknown error occurred",
         status: "error",
       });
     }
   };
 
   return (
-    <VStack spacing={4} align="stretch">
-      <Input
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <Input
-        placeholder="Password"
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <Button onClick={handleLogin}>Log In</Button>
-    </VStack>
+    <Container
+      centerContent
+      maxWidth="md"
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      height="100vh"
+    >
+      <VStack spacing={4} align="stretch">
+        <Input
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          placeholder="Password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Button onClick={handleLogin} colorScheme="blue">
+          Log In
+        </Button>
+      </VStack>
+    </Container>
   );
 };
 
 export default LoginPage;
+
+const getErrorDescription = (error: Error) => {
+  switch (error.message) {
+    case "Firebase: Error (auth/invalid-email).":
+      return "Invalid Email or Password";
+    default:
+      return "Some error occurred";
+  }
+};
