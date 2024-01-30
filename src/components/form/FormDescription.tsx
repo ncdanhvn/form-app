c619509ddd0113bc02b154a502f9b62470c2c820
Danh Nguyen <ncdanhvn@gmail.com>
2024-01-30 15:41:32 +0700

    Revisit Form Description

--- src/components/form/FormDescription.tsx
+++ src/components/form/FormDescription.tsx
@@ -1,13 +1,26 @@
 import { Box, Text, VStack } from "@chakra-ui/react";
+import useDescriptionToolbarStore from "../../stores/toolbarStore/descriptionToolbarStore";
 
 interface Props {
   description: string;
 }
 
 const FormDescription = ({ description }: Props) => {
+  const { bold, italic, underline, align, textColor, fontFamily, fontSize } =
+    useDescriptionToolbarStore();
+
   return (
-    <Box textAlign={"center"} color={"blue.600"}>
-      {description}
+    <Box display={"flex"} justifyContent={align} w={"100%"} px={8} py={4}>
+      <Text
+        fontSize={`${fontSize}`}
+        fontFamily={`${fontFamily}, sans-serif`}
+        fontWeight={bold ? "bold" : "normal"}
+        fontStyle={italic ? "italic" : "normal"}
+        textDecoration={underline ? "underline" : "normal"}
+        color={textColor}
+      >
+        {description}
+      </Text>
     </Box>
   );
 };
