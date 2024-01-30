fedfed078508af8d0b35b9566f5dae2a80d9c6c0
Danh Nguyen <ncdanhvn@gmail.com>
2024-01-30 15:34:13 +0700

    Revisit Form Title

--- src/components/form/FormTitle.tsx
+++ src/components/form/FormTitle.tsx
@@ -1,13 +1,31 @@
 import { Box, Heading } from "@chakra-ui/react";
+import useCanvasStore from "../../stores/canvasStore";
+import useTitleToolbarStore from "../../stores/toolbarStore/titleToolbarStore";
 
 interface Props {
   title: string;
 }
 
 const FormTitle = ({ title }: Props) => {
+  const {
+    title: { backgroundColor },
+  } = useCanvasStore();
+
+  const { bold, italic, underline, align, textColor, fontFamily, fontSize } =
+    useTitleToolbarStore();
+
   return (
-    <Box bg="pink.500" color="white" w={"100%"} textAlign={"center"} py={8}>
-      <Heading as="h1" size="xl" fontFamily="Grape Nuts, sans-serif">
+    <Box bg={backgroundColor} w={"100%"} py={8} px={8}>
+      <Heading
+        as="h1"
+        fontSize={`${fontSize}`}
+        fontFamily={`${fontFamily}, sans-serif`}
+        fontWeight={bold ? "bold" : "normal"}
+        fontStyle={italic ? "italic" : "normal"}
+        textDecoration={underline ? "underline" : "normal"}
+        textAlign={align}
+        color={textColor}
+      >
         {title}
       </Heading>
     </Box>
